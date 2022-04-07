64947dc1a01b2c153daad2cc5a15e73dc639b88f
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-04-07 19:28:55 +0530

    Time: 28 ms (72.17%), Space: 10.5 MB (62.54%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,15 +1,31 @@
 const int MOD = 1e9+7;
 class Solution {
 public:
-    int threeSumMulti(vector<int>& arr, int X) {
-        int n = arr.size(), mod = 1e9+7, ans = 0;
-        unordered_map<int, int> m;
-        
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size();
+        map<int, int> mp;
+        long int ans = 0;
         for(int i=0; i<n; i++) {
-            ans = (ans + m[X - arr[i]]) % mod;
-            
-            for(int j=0; j<i; j++) m[arr[i] + arr[j]]++;
+            mp[arr[i]]++;
         }
+        for(auto i: mp) {
+            for(auto j: mp) { 
+                int k = target - i.first - j.first;
+                if(!mp.count(k)) {
+                    continue;
+                }
+                else if(i.first == j.first && j.first == k) {
+                    ans = (ans + 1LL*mp[i.first]*(mp[i.first]-1)*(mp[i.first]-2)/6)%MOD;
+                }
+                else if(i.first == j.first && j.first !=k) {
+                    ans = (ans + 1LL*mp[i.first]*(mp[i.first]-1)*mp[k]/2)%MOD;
+                }
+                else if(i.first < j.first && j.first < k) {
+                    ans = (ans + 1LL*mp[i.first]*mp[j.first]*mp[k])%MOD;
+                }
+            }
+        }
+        ans %= MOD;
         return ans;
     }
 };
