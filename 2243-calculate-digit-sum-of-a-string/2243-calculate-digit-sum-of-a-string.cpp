8ac4b19adf26d0d894626827d597119d80ffa1d7
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-04-17 10:00:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (83.33%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.cpp
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     string digitSum(string s, int k) {
-        while ((int) s.size() > k) {
-            int sum = 0;
-            string t = "";
-            int count = 0;
-            for (int i = 0; i < (int) s.size(); i++) {
-                sum += (s[i] - '0');
-                ++count;
-                if (count == k) {
-                    t += to_string(sum);
+        int cnt = 0;
+        int sum = 0;
+        string temp = "";
+        while(s.size() > k) {
+            cnt = 0;
+            temp = "";
+            for(int i=0; i<s.size(); i++) {
+                cnt++;
+                sum += s[i]-'0';
+                if(cnt == k) {
+                    cnt = 0;
+                    temp += to_string(sum);
                     sum = 0;
-                    count = 0;
                 }
             }
-            if (count > 0) {
-                t += to_string(sum);
+            if(cnt!=0) {
+                temp+=to_string(sum);
+                sum = 0;
             }
-            s = t;
-        }
-        return s;
+            s = temp;
+        } 
+        return s;   
     }
 };
