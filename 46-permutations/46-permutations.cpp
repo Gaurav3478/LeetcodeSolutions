124d98acd736fe142b80ed839aa4ae9048a50c51
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-04-28 15:05:20 +0530

    Time: 7 ms (31.77%), Space: 8.4 MB (12.71%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,31 @@
+//this will be the solution using extra space
+
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans, int l, int r, vector<int> nums) {
-        if(l == r) {
-            ans.push_back(nums);
+    void solve(vector<vector<int>> &ans, vector<int> nums, vector<int> &ds, vector<int> temp, int n) {
+        if(ds.size() == n) {
+            ans.push_back(ds);
             return;
         }
-        for (int i = l; i <= r; i++)
-        {
- 
-            // Swapping done
-            swap(nums[l], nums[i]);
- 
-            // Recursion called
-            solve(ans, l+1, r, nums);
- 
-            //backtrack
-            swap(nums[l], nums[i]);
+        for(int i = 0; i<n; i++) {
+            if(!temp[i]) {
+                temp[i] = 1;
+                ds.push_back(nums[i]);
+                solve(ans, nums, ds, temp, n);
+                temp[i] = 0;
+                ds.pop_back();
+            }
         }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int l = 0, r = nums.size() - 1;
-        solve(ans, l, r, nums);
+        vector<int> ds;
+        int n = nums.size();
+        vector<int> temp(n);
+        // for(int i = 0; i<temp.size(); i++) {
+        //     temp[i] = 0;
+        // }
+        solve(ans, nums, ds, temp, n);
         return ans;
     }
 };
