0c957357ce7ca88612f905db67935490b7adf8ba
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-11 01:38:47 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (87.15%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+private:
+    queue<int> q;
+public:
+    MyStack() {
+    }
+    void push(int x) {
+        int s = q.size();
+        q.push(x);
+        for (int i = 0; i < s; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int n = q.front();
+        q.pop();
+        return n;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.size() == 0;
+    }
+};
