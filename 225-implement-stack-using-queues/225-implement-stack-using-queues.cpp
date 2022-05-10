333ee4e3ba5ec876ec520598db5d7aeb1355dabd
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-11 01:41:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (55.46%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -6,11 +6,11 @@ public:
     }
     void push(int x) {
         int s = q.size();
-        q.push(x);
         for (int i = 0; i < s; i++) {
             q2.push(q.front());
             q.pop();
         }
+        q.push(x);
         for(int i=0; i<s; i++) {
             q.push(q2.front());
             q2.pop();
