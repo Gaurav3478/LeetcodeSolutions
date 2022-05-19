d8bcc98cc69124d40f73708a252dd9682d75496b
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-19 14:08:40 +0530

    Time: 129 ms (68.00%), Space: 67.8 MB (11.88%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-//         int n = nums.size();
-//         int max_so_far, current_sum = 0;
-//         current_sum = nums[0];
-//         max_so_far = current_sum;
-        
-//         for(int i=1; i<n; i++) {
-//             if(current_sum + nums[i] > 0) {
-//                 current_sum = max(current_sum + nums[i], nums[i]);
-//             }
-//             else {
-//                 current_sum = nums[i];
-//             }
-//             if(current_sum > max_so_far) {
-//                 max_so_far = current_sum;
-//             }
-//         }
-//         return max_so_far;
         int n = nums.size();
-        int sum_so_far = 0;
-        int ans = nums[0];
+        int localMax = 0, globalMax = INT_MIN;
         for(int i=0; i<n; i++) {
-            if(sum_so_far + nums[i] > 0) {
-                sum_so_far = max(sum_so_far + nums[i], nums[i]);
+            localMax = max(nums[i], localMax + nums[i]);
+            if(localMax > globalMax) {
+                globalMax = localMax;
             }
-            else {
-                sum_so_far = nums[i];
-            }
-            ans = max(ans, sum_so_far);
         }
-        return ans;
+        return globalMax;
     }
 };
