da5df90660013d253003156db60b33c460934be9
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-21 13:04:04 +0530

    Time: 275 ms (86.59%), Space: 144.6 MB (72.82%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -1,14 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+ll dfs(ll v) {
+    ll mn = c[v];
+    vis[v] = true;
+    for(ll x: adj[v]) {
+        if(!vis[x]) {
+            mn = min(mn, dfs(x));
+        }
+    }
+    return mn;
+}
+*/
+
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (root == NULL) 
-            return 0;
-        if (root->left == NULL && root->right == NULL) // Reach to leaf node
-            return 1; 
-        if (root->left == NULL) 
-            return minDepth(root->right) + 1;
-        if (root->right == NULL) 
-            return minDepth(root->left) + 1;
-        return min(minDepth(root->left), minDepth(root->right)) + 1;
+        if(root == NULL) {
+            return 0;            
+        }
+        queue<TreeNode *> q;
+        q.push(root);
+        int depth = 1;
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i=0; i<n; i++) {
+                TreeNode *t = q.front();
+                if(t->left == NULL && t->right == NULL) {
+                    return depth;
+                }
+                q.pop();
+                if(t->left) {
+                    q.push(t->left);
+                }
+                if(t->right) {
+                    q.push(t->right);
+                }
+            }
+            depth++;
+        }
+        return -1;
     }
 };
