54785cb3935f3f05eb0441f591a72d87d42f3cdd
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-22 12:49:27 +0530

    Time: 773 ms (33.33%), Space: 100.7 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int minimumLines(vector<vector<int>>& sp) {
-        sort(sp.begin(), sp.end());
-        if (sp.size() <= 1) return 0;
-        long double prevSlope = (sp[1][1] - sp[0][1]) / (long double)(sp[1][0] - sp[0][0]);
-        int lines = 1;
-        for (int i = 2; i < sp.size(); i++) {
-            long double currentSlope = (sp[i][1] - sp[i-1][1]) / (long double)(sp[i][0] - sp[i-1][0] );
-            if (currentSlope != prevSlope) {
-                lines++;
-                prevSlope = currentSlope;
-            }
+    int minimumLines(vector<vector<int>>& stockPrices) {
+      int n = stockPrices.size();
+      if(n==1) {
+        return 0;
+      }
+      sort(stockPrices.begin(), stockPrices.end());
+      int ans = 1;
+      long double prevGrad = (stockPrices[1][1] - stockPrices[0][1])/(long double)(stockPrices[1][0] - stockPrices[0][0]);
+      for(int i=1; i<n-1; i++) {
+        long double curGrad = (stockPrices[i+1][1] - stockPrices[i][1])/(long double)(stockPrices[i+1][0] - stockPrices[i][0]);
+        if(prevGrad != curGrad) {
+          ans++;
         }
-        return lines;
+        prevGrad = curGrad;
+      }
+      return ans;
     }
 };
