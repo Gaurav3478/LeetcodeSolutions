4bfc005bf11de8adaf9205961db5294436ea8aa8
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-22 12:47:21 +0530

    Time: 512 ms (66.67%), Space: 100.6 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int minimumLines(vector<vector<int>>& st) {
-         sort(st.begin(),st.end());
-        
-        long double m=INT_MIN;
-        int ans=0;
-        for(int i=0;i<st.size()-1;i++)
-        {
-            long double x=(st[i+1][0]-st[i][0]);
-            long double y=(st[i+1][1]-st[i][1]);
-            
-            long double p=y/x;
-            if(p!=m)
-            {
-                m=p;
-                ans++; 
+    int minimumLines(vector<vector<int>>& sp) {
+        sort(sp.begin(), sp.end());
+        if (sp.size() <= 1) return 0;
+        long double prevSlope = (sp[1][1] - sp[0][1]) / (long double)(sp[1][0] - sp[0][0]);
+        int lines = 1;
+        for (int i = 2; i < sp.size(); i++) {
+            long double currentSlope = (sp[i][1] - sp[i-1][1]) / (long double)(sp[i][0] - sp[i-1][0] );
+            if (currentSlope != prevSlope) {
+                lines++;
+                prevSlope = currentSlope;
             }
         }
-        return ans;
-        
+        return lines;
     }
 };
