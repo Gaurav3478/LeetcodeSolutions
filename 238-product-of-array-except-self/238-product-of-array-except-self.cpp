9d1794c157c67015dec2192db87afa12f6038cc5
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-25 10:48:25 +0530

    Time: 56 ms (7.13%), Space: 24 MB (81.79%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftProduct(n), rightProduct(n);
-        for(int i=0; i<n; i++) {
-            if(i==0) {
-                leftProduct[i] = nums[i];
-            }
-            else {
-                leftProduct[i] = leftProduct[i-1]*nums[i];
-            }
+        int n=nums.size();
+        int fromBegin=1;
+        int fromLast=1;
+        vector<int> res(n,1);
+        
+        for(int i=0;i<n;i++){
+            res[i]*=fromBegin;
+            fromBegin*=nums[i];
         }
-        for(int i=n-1; i>=0; i--) {
-            if(i==n-1) {
-                rightProduct[i] = nums[i];
-            }
-            else {
-                rightProduct[i] = rightProduct[i+1]*nums[i];
-            }
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            res[i]*=fromLast;
+            fromLast*=nums[i];
         }
-        for(auto it: leftProduct) {
-            cout << it << " ";
-        }
-        cout << endl;
-        for(auto it: rightProduct) {
-            cout << it << " ";
-        }
-        vector<int> ans;
-        for(int i=0; i<n; i++) {
-            if(i==0) {
-                ans.push_back(rightProduct[1]);
-            }
-            else if(i==n-1) {
-                ans.push_back(leftProduct[n-2]);
-            }
-            else {
-                ans.push_back(leftProduct[i-1] * rightProduct[i+1]);
-            }
-        }
-        return ans;
+        return res;
     }
 };
