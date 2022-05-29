91a74d7b265561c38492c375bf152788f1d5b04c
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-05-29 20:15:28 +0530

    Time: 203 ms (33.33%), Space: 26.2 MB (100.00%) - LeetHub

---
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string solve(string &pri,int discount){
+        long int price = stol(pri);
+        pri = "";
+        double ans = price - ((double)price/100)*discount;
+        ostringstream out;
+        out.precision(2);
+        out << std::fixed << ans;
+        return out.str();
+    }
+    string discountPrices(string nums, int discount) {
+        string ans = "", num = "";
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == '$' && (i==0 || nums[i-1]==' ')){
+                ans += nums[i];
+                int j = i+1;
+                if(j >= nums.size()) return ans;
+                while(j<nums.size() && isdigit(nums[j])){
+                    num += nums[j];
+                    j++;
+                }
+                if(j>=nums.size() || (nums[j] == ' ' && num.size())){
+                    string val = solve(num,discount);
+                    ans += val;
+                }else{
+                    ans += num;
+                    num = "";
+                }
+                i = j-1;
+            }else{
+                ans += nums[i];
+            }
+        }
+        return ans;
+    }
+};
