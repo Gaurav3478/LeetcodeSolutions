1a73f42da83e7695b39df7232ab64d29f80a50b8
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-03 02:00:09 +0530

    Time: 7 ms (27.71%), Space: 15.3 MB (26.21%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        //try using bitmask
-        int n = nums.size();
-        //=> 2^n subsets
-        vector<vector<int>> ans;
-        for(int i=0; i<1<<n; i++) {
-            //i is my bitmask 
-            vector<int> temp;
-            for(int j=0; j<n; j++) {
-                if(i & 1<<j) {
-                    temp.push_back(nums[j]);
-                }
-            }
+    void solve(vector<vector<int>> &ans, int index, vector<int> &temp, vector<int> nums, int n) {
+        if(index == n) {
             ans.push_back(temp);
+            return;
         }
+        //we take that index
+        solve(ans, index+1, temp, nums, n);
+        temp.push_back(nums[index]);
+        solve(ans, index+1, temp, nums, n);
+        temp.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //in general, we'll take then skip then take then skip
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int n = nums.size();
+        solve(ans, 0, temp, nums, n);
         return ans;
     }
 };
