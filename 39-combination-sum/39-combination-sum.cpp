11cbc87408e54fe5778d4e8e55fb51d65af8aff5
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-03 10:17:21 +0530

    Time: 11 ms (62.87%), Space: 11.1 MB (49.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans, int sum, vector<int> &temp, int index, vector<int> candidates, int target) {
-        if(sum > target) {
+    void solve(vector<vector<int>> &ans, vector<int> &temp, int index, int curr_sum, vector<int> &candidates, int target, int n) {
+        if(index >= n || curr_sum > target) {
             return;
         }
-        if(index == candidates.size()) {
-            if(sum == target) {
-                ans.push_back(temp);
-            }
+        if(curr_sum == target) {
+            ans.push_back(temp);
             return;
         }
+        //include 
+        curr_sum += candidates[index];
         temp.push_back(candidates[index]);
-        solve(ans, sum + candidates[index], temp, index, candidates, target);
+        solve(ans, temp, index, curr_sum, candidates, target, n);
+        curr_sum -= candidates[index];
         temp.pop_back();
-        solve(ans, sum, temp, index + 1, candidates, target);
+        solve(ans, temp, index + 1, curr_sum, candidates, target, n);
     }
-    
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        //we will either include or not include
         vector<vector<int>> ans;
+        int n = candidates.size();
         vector<int> temp;
-        solve(ans, 0, temp, 0, candidates, target);
-        return ans;
+        solve(ans, temp, 0, 0, candidates, target, n);
+        //ans temp index curr_sum candidates required_sum array_size
+        return ans;    
     }
 };
