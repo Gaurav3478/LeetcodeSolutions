4df0cf393e74f7d6aaa43ce721b5d617811424da
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-03 12:49:40 +0530

    Time: 7 ms (50.21%), Space: 13.7 MB (47.77%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    void solve(vector<string> &ans, string cur, int open, int close, int max) {
-        if(cur.size() == 2*max) {
+    void solve(vector<string> &ans, string cur, int n, int open, int close) {
+        if(cur.size() == 2*n) {
             ans.push_back(cur);
             return;
         }
-        if(open < max) {
-            solve(ans, cur + "(", open + 1, close, max);
+        //we want to take the left bracket first followed by right bracket
+        if(open < n) {
+            solve(ans, cur + '(', n, open+1, close);
         }
         if(close < open) {
-            solve(ans, cur + ")", open, close + 1, max);
+            solve(ans, cur + ')', n, open, close + 1);
         }
+
     }
-    
+
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        solve(ans, "", 0, 0, n);
+        solve(ans, "", n, 0, 0);
         return ans;
     }
 };
