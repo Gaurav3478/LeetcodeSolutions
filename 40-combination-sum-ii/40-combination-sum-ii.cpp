c76779a6ffe5d2490a7317c2c4841f8751ec54a2
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-03 11:05:54 +0530

    Time: 158 ms (5.02%), Space: 104.8 MB (5.06%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void solve(vector<vector<int>> &ans, vector<int> &temp, int index, int sum, vector<int> candidates, int target) {
-        if(index == candidates.size()) {
-            if(sum == target) {
-                ans.push_back(temp);
-            }
+        if(sum > target) {
+            return;
+        }
+        if(sum == target) {
+            ans.push_back(temp);
+            return;
+        }
+        if(index >= candidates.size()) {
             return;
         }
         temp.push_back(candidates[index]);
-        if(sum + candidates[index] <= target) {
-            solve(ans, temp, index + 1, sum + candidates[index], candidates, target);
-        }
+        solve(ans, temp, index + 1, sum + candidates[index], candidates, target);
         int j = index;
         while(index < candidates.size() && candidates[index] == candidates[j]) {
             index++;
