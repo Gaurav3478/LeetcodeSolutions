5129a7c44bbcb3d2d68d562690f86a0dc8c5bd88
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-05 02:11:57 +0530

    Time: 10 ms (41.44%), Space: 9 MB (41.16%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,30 +2,15 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n);
-        //dp[i][j] represents the minimum cost to get to that point
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         for(int i=0; i<n; i++) {
-            dp[i].clear();
-            dp[i].resize(triangle[i].size());
-            for(int j=0; j<triangle[i].size(); j++) {
-                if(i==0 && j==0) {
-                    dp[i][j] = triangle[i][j];
-                }
-                else if(j == triangle[i-1].size()) {
-                    dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-                }
-                else if(j == 0) {
-                    dp[i][j] = triangle[i][j] + dp[i-1][j];
-                }
-                else {
-                    dp[i][j] = triangle[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
-                }
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        for(int i=n-2; i>=0; i--) {
+            for(int j=i; j>=0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
-        int ans = INT_MAX;
-        for(int i=0; i<triangle[n-1].size(); i++) {
-            ans = min(ans, dp[n-1][i]);
-        }
-        return ans;
+        return dp[0][0];
     }
 };
