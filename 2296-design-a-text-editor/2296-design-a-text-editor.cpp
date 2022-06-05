3944fc4da90a64b5584977572bc12569ffeee163
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-05 12:46:47 +0530

    Time: 495 ms (11.11%), Space: 112.8 MB (33.33%) - LeetHub

---
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -0,0 +1,44 @@
+class TextEditor {
+public:
+    string before, after;
+    
+    TextEditor() {     
+    }
+    
+    void addText(string text) {
+        before.insert(end(before), begin(text), end(text));
+    }
+    
+    int deleteText(int k) {
+        k = min(k, (int)before.size());
+        before.resize(before.size() - k);
+        return k;
+    }
+    
+    string cursorLeft(int k) {
+        while(k-- && !before.empty()) {
+            after.push_back(before.back());
+            before.pop_back();
+        }
+        int x = min((int)before.size(), 10);
+        return before.substr(before.size() - x, x);
+    }
+    
+    string cursorRight(int k) {
+        while(k-- && !after.empty()) {
+            before.push_back(after.back());
+            after.pop_back();
+        }
+        int x = min((int)before.size(), 10);
+        return before.substr(before.size() - x, x);
+    }
+};
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor* obj = new TextEditor();
+ * obj->addText(text);
+ * int param_2 = obj->deleteText(k);
+ * string param_3 = obj->cursorLeft(k);
+ * string param_4 = obj->cursorRight(k);
+ */
