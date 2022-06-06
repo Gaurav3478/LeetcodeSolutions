59839aa34f0eec5d7f9e0d91c63455901c977412
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-06 11:37:20 +0530

    Time: 186 ms (21.93%), Space: 45.2 MB (19.76%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        //take donttake
-        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        vector<vector<int>> dp(n, vector<int>(amount+1, 1e9));
+        //what's our base case
         for(int i=0; i<=amount; i++) {
-            if(i%coins[0] == 0 ) {
-                dp[0][i] = i/coins[0];
+            if(i%coins[0] == 0) {
+                dp[0][i] = i/coins[0];   
             }
             else {
-                dp[0][i] = 1e9;
-            }
-        }                   
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<=amount; j++) {
-                int donttake = dp[i-1][j];
-                int take = 1e9;
-                if(coins[i] <= j) {
-                    take = 1 + dp[i][j-coins[i]];
-                }
-                dp[i][j] = min(take, donttake);
+               dp[0][i] = 1e9;
             }
         }
-        return dp[n-1][amount] >= 1e9?-1:dp[n-1][amount];
+        for(int index=1; index<n; index++) {
+            for(int value=0; value<=amount; value++) {
+                //donttake
+                int donttake = dp[index-1][value];
+                //take
+                int take = INT_MAX;
+                if(coins[index] <= value) {
+                    take = 1 + dp[index][value - coins[index]];
+                }
+                dp[index][value] = min(take, donttake);
+            }
+        }
+        if(dp[n-1][amount] >= 1e9) {
+            return -1;
+        }
+        else {
+            return dp[n-1][amount];            
+        }
     }
 };
