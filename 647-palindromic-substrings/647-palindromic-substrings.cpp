fb200c26d7e9e67feaffd9438303e2f1c171a2e6
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-15 15:54:12 +0530

    Time: 45 ms (24.26%), Space: 7.6 MB (47.85%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool is_palindrome(string s) {
-        int n = s.size();
-        for(int i=0; i<=n/2 - 1; i++) {
-            if(s[i] != s[n-1-i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     int countSubstrings(string s) {
         int n = s.size();
-        vector<int> dp(n, 0);
-        dp[0] = 1;
-        for(int i=1; i<n; i++) {
-            //dp[i] = dp[i-1] + # palindromes ending at i
-            dp[i] = dp[i-1] + 1;
-            for(int j=0; j<i; j++) {
-                if(is_palindrome(s.substr(j, i-j+1))) {
-                    dp[i]++;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int i=0; i<n; i++) {
+            dp[i][i] = true;
+        }
+        int count = n;
+        for(int len = 2; len <= n; len++) {
+            for(int i=0; i+len -1<=n-1; i++) {
+                int j = i + len -1;
+                if(s[i] != s[j]) {
+                    dp[i][j] = false;
                 }
+                else {
+                    if(len == 2) {
+                        dp[i][j] = true;
+                    }
+                    else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+                count += dp[i][j];
             }
         }
-        return dp[n-1];
+        return count;
     }
 };
