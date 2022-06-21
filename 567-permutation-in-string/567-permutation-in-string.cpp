25243aa2c6217530b69dd5c28a0c398c70a75cf7
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-21 22:40:38 +0530

    Time: 13 ms (56.92%), Space: 7.4 MB (47.08%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s2.size();
-        map<char, int> mp1, mp2;
+        vector<int> mp1(26, 0), mp2(26, 0);
         int k = s1.size();
         if(k > n) {
             return false;
         }
         for(int i=0; i<k; i++) {
-            mp1[s1[i]]++;
+            mp1[s1[i] - 'a']++;
         }
         for(int i=0; i<k; i++) {
-            mp2[s2[i]]++;
+            mp2[s2[i] - 'a']++;
         }
         if(mp1 == mp2) {
             return true;
         }
         for(int i=k; i<n; i++) {
-            if(mp2[s2[i-k]] == 1) {
-                mp2.erase(s2[i-k]);
-            }
-            else {
-                mp2[s2[i-k]]--;
-            }
-            mp2[s2[i]]++;
+            mp2[s2[i-k] - 'a']--;
+            mp2[s2[i] - 'a']++;
             if(mp1 == mp2) {
                 return true;
             }
