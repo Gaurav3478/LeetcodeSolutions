9445cffb26740c67a37804989c11661976e5f5df
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-22 11:10:02 +0530

    Time: 351 ms (53.58%), Space: 57.1 MB (93.09%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b) {
-        return sqrt(a[0]*a[0] + a[1]*a[1]) < sqrt(b[0]*b[0] + b[1]*b[1]);
-    }
-    
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), cmp);
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b) {
+            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
+        });
         return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
