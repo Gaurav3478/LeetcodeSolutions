d5218879b0b4950fe6ebd96dee23ed7a33ebc82b
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-28 14:22:42 +0530

    Time: 282 ms (10.56%), Space: 14.6 MB (62.48%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        typedef vector<int>::iterator vi;
+        
+        struct comp {
+            bool operator()(pair<vi, vi> p1, pair<vi, vi> p2) {
+                return *p1.first > *p2.first;
+            }
+        };
+        
+        int lo = INT_MAX, hi = INT_MIN;
+        priority_queue<pair<vi, vi>, vector<pair<vi, vi>>, comp> pq;
+        for (auto &row : nums) {
+            lo = min(lo, row[0]);
+            hi = max(hi, row[0]);
+            pq.push({row.begin(), row.end()});
+        }
+        vector<int> ans = {lo, hi};
+        while (true) {
+            auto p = pq.top();
+            pq.pop();
+            ++p.first;
+            if (p.first == p.second)
+                break;
+            pq.push(p);
+            
+            lo = *pq.top().first;
+            cout << lo << endl;
+            hi = max(hi, *p.first);
+            cout << hi << endl;
+            if (hi - lo < ans[1] - ans[0])
+                ans = {lo, hi};
+        }
+        return ans;
+    }
+};
