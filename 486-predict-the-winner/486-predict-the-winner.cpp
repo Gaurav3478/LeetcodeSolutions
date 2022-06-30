369c2296a2e0ca8a2e2cceb754251be6dc301305
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-06-30 09:59:40 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (23.85%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,21 +1,14 @@
 class Solution {
-    bool checkWin(int ans,int total){
-	   //ans=max player 1 can score and total=sum of all elements in the array
-	   //so, player2's score=total-player1's score . ie:-total-ans;
-	   //According to question player1's score should be greater than or equal to player2's score. 
-	   return ans>=total-ans;
-    }
-    int maxScore(vector<int>&A,int total,int i,int j){
-        if(i>j) //our base condition
-            return 0;
-      return total-min(maxScore(A,total-A[i],i+1,j),maxScore(A,total-A[j],i,j-1)); //max the current player can get is total-maxm the next player can get.
-    }
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        int total=0; 
-		//total=sum of all elements in the array
-        for(auto x:nums)
-            total+=x;
-        return checkWin(maxScore(nums,total,0,nums.size()-1),total);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = 0; i < n; i++) dp[i][i] = nums[i];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j+i < n; j++) {
+                dp[j][j+i] = max(nums[j+i]-dp[j][j+i-1], nums[j]-dp[j+1][j+i]);
+            }
+        }
+        return dp[0][n-1] >= 0;
     }
 };
