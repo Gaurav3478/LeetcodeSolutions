3d56ae696e0bfc21650b20682e2ccaee9e4b3cdb
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-07-03 00:20:21 +0530

    Time: 8 ms (6.89%), Space: 7.9 MB (5.99%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        //all that matters in our current choice is whether we took the last element or not
+        //for each house, make a choice: take or don't take
         int n = nums.size();
-        vector<int> dp(n, 0); 
+        vector<int> dp(n, 0);
+        //dp[i] represents the maximum possible value given that we are ending at index i
         dp[0] = nums[0];
-        for(int i=1; i<n; i++) {
-            //dont steal
-            int dontsteal = dp[i-1];
-            //steal
-            int steal = nums[i];
-            if(i > 1) {
-                steal = dp[i-2] + nums[i];
-            }
-            dp[i] = max(steal, dontsteal);
-        }        
+        if(n > 1) {
+            dp[1] = max(nums[0], nums[1]);
+        }
+        for(int i=2; i<n; i++) {
+            int donttake = dp[i-1];
+            int take = dp[i-2] + nums[i];
+            dp[i] = max(take, donttake);
+        }
         return dp[n-1];
     }
 };
