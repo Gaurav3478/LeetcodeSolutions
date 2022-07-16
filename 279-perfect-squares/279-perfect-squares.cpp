5369f1ec2e53487acdabccf73102ae1c9f7f3ee0
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-07-16 14:26:43 +0530

    Time: 1623 ms (5.01%), Space: 151.3 MB (7.83%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> perfectSquares;
+        for(int i=1; i<=sqrt(n); i++) {
+            perfectSquares.push_back(i*i);
+        }
+        int sz = perfectSquares.size();
+        vector<vector<int>> dp(sz, vector<int>(n+1, -1));
+        for(int i=1; i<=n; i++) {
+            if(i%perfectSquares[0] == 0) {
+                dp[0][i] = i/perfectSquares[0];
+            }
+        }
+        for(int i=0; i<sz; i++) {
+            dp[i][0] = 0;
+        }
+        for(int i=1; i<sz; i++) {
+            for(int j=1; j<=n; j++) {
+                int donttake = dp[i-1][j];
+                int take = INT_MAX;
+                if(j >= perfectSquares[i] && dp[i][j - perfectSquares[i]] != -1) {
+                    take = 1 + dp[i][j - perfectSquares[i]];
+                }
+                dp[i][j] = min(take, donttake);
+            }
+        }
+        // for(auto it: dp) {
+        //     for(auto jt: it) {
+        //         cout << jt << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << endl;
+        return dp[sz-1][n];
+    }
+};
