92fad40a01d322ddb0f9b58703bb6d67483ed6b7
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-07-23 10:02:48 +0530

    Time: 1633 ms (13.10%), Space: 150.5 MB (57.83%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,18 +1,21 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
 using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        ordered_set s;
-        vector<int>ans;
-        int n=nums.size();
-        for(int i=n-1;i>=0;i--){
-            ans.push_back(s.order_of_key(nums[i]));
-            s.insert(nums[i]);
+        ordered_set<pair<int, int>> os;
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i=n-1; i>=0; i--) {            
+            ans[i] = os.order_of_key({nums[i], 0});
+            os.insert({nums[i], i});
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
