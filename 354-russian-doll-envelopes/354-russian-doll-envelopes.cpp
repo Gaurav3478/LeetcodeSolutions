5b6ee2adc064cbaa5d0566afd2795c8a4e0dcb5d
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-07-26 18:15:04 +0530

    Time: 592 ms (39.47%), Space: 77.5 MB (62.83%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
-        sort(envelopes.begin(), envelopes.end(), [&] (vector<int> &a, vector<int> &b){
+        sort(envelopes.begin(), envelopes.end(), [] (vector<int> &a, vector<int> &b){
             if(a[0] == b[0]) {
                 return a[1] > b[1];
             }
             return a[0] < b[0];
         });
-        //now we simply have to find LIS on the heights of the envelopes
         vector<int> temp;
         temp.push_back(envelopes[0][1]);
         for(int i=1; i<n; i++) {
