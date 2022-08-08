aff1330ff3548645e0f3a0ab4bedfa6c776509fb
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-08-08 17:48:48 +0530

    Time: 17 ms (5.12%), Space: 9.5 MB (51.76%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,33 +3,39 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        //binary search on row first then on column
-        int low = 0, high = n-1;
-        int mid;
-        while (low < high) {
-            mid = low + (high - low)/2;
-            // moving l up if needed
-            if (matrix[mid].back() < target) low = mid + 1;
-            // moving r down if needed
-            else if (matrix[mid][0] > target) high = mid - 1;
-            // we found our row!
-            else {
-                low = mid;
-                break;
-            };
-        }
-        int row = low;
-        low = 0, high = m-1;
+        //first find the row, then find the column
+        int row = -1;
+        int low = 0, high = n - 1;
         while(low <= high) {
-            mid = low + (high - low)/2;
-            if(matrix[row][mid] > target) {
+            int mid = low + (high - low)/2;
+            int val = matrix[mid][m-1];
+            if(val == target) {
+                row = mid;
+                break;
+            }         
+            else if(val > target) {
+                row = mid;
                 high = mid - 1;
             }
-            else if(matrix[row][mid] < target) {
+            else {
                 low = mid + 1;
             }
-            else {
+        }
+        if(row == -1) {
+            return false;
+        }
+        low = 0, high = m-1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int val = matrix[row][mid];
+            if(val == target) {
                 return true;
+            }
+            else if(val > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
         return false;
