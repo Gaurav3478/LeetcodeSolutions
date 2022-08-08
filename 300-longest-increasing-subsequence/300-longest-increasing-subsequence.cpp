b81456a854d1943548d8ce45d489e0b9e31943dc
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-08-08 12:45:27 +0530

    Time: 588 ms (27.15%), Space: 10.8 MB (21.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,15 +3,35 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 1);
+        vector<int> prev(n, -1);
         for(int i=0; i<n; i++) {
             for(int j=0; j<i; j++) {
                 if(nums[j] < nums[i]) {
                     if(1 + dp[j] > dp[i]) {
                         dp[i] = 1 + dp[j];
+                        prev[i] = j;
                     }
                 }
             }
         }
+        int maxi = *max_element(dp.begin(), dp.end());
+        int idx;
+        for(int i=n-1; i>=0; i--) {
+            if(dp[i] == maxi) {
+                idx = i;
+                break;
+            }
+        }
+        vector<int> ans;
+        while(idx != -1) {
+            ans.push_back(nums[idx]);
+            idx = prev[idx];
+        }
+        reverse(ans.begin(), ans.end());
+        for(auto ele: ans) {
+            cout << ele << " ";
+        }
+        cout << endl;
         return *max_element(dp.begin(), dp.end());
     }
 };
