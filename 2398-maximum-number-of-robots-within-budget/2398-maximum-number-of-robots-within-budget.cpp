6ec47c41ed344be9cddcf33eafaad8343845c109
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-09-04 16:00:37 +0530

    Time: 2742 ms (10.00%), Space: 516.6 MB (10.00%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
+    int maxN = 5e4 + 1;
+    int K = 25;
     int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
+        vector<vector<int>> sp(maxN, vector<int>(K + 1, 0));
         int n = chargeTimes.size();
-        int i = 0, j = 0;
-        multiset<long long int> ms;
-        long long int val = 0;
-        int cnt = 0;
-        while(j < n) {
-            ms.insert(chargeTimes[j]);
-            val += runningCosts[j];
-            while(!ms.empty() && *ms.rbegin() + (j - i + 1)*val > budget) {
-                val -= runningCosts[i];
-                ms.erase(ms.find(chargeTimes[i]));
-                i++;
+        for(int i=0; i<n; i++) {
+            sp[i][0] = chargeTimes[i];
+        }
+        for(int j=1; j<=K; j++) {
+            for(int i=0; i + (1 << j) - 1 < n; i++) {
+                sp[i][j] = max(sp[i][j-1], sp[i + (1 << (j - 1))][j - 1]);
             }
-            cnt = max(cnt, j - i + 1);
+        }
+        
+        int lg[maxN+1];
+        lg[1] = 0;
+        for (int i = 2; i <= maxN; i++)
+            lg[i] = lg[i/2] + 1;
+        
+        int i = 0, j = 0;
+        long long int val = 0;
+        int ans = 0;
+        while(j < n) {
+            val += runningCosts[j];
+            int len = lg[j - i + 1];
+            while((j - i + 1)*val + max(sp[i][len], sp[j - (1 << len) + 1][len]) > budget) {
+                val -= runningCosts[i];
+                i++;
+                if(i >= j) {
+                    break;
+                }
+                len = lg[j - i + 1];
+            }
+            ans = max(ans, j - i + 1);
             j++;
         }
-        return cnt;
+        return ans;
     }
 };
