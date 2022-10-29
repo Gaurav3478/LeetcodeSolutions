0fcb8d84ee7b4b4df0463a6461c00d748b8e8218
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-10-29 23:23:02 +0530

    Time: 1862 ms (12.50%), Space: 180.5 MB (12.50%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -1,8 +1,6 @@
-#include <ext/pb_ds/assoc_container.hpp>
+#include "ext/pb_ds/assoc_container.hpp"
 using namespace __gnu_pbds;
-
-template <typename T> using ordered_set = tree<T, null_type,
-	less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T>using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 class Solution {
 public:
