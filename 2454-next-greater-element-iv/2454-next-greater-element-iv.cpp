fbfdec914d9a9ad9f5f3be6782ef926314ae25a6
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-10-30 17:44:15 +0530

    Time: 577 ms (12.50%), Space: 153.9 MB (12.50%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -1,7 +1,3 @@
-#include "ext/pb_ds/assoc_container.hpp"
-using namespace __gnu_pbds;
-template<class T>using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
 class Solution {
 public:
     vector<int> secondGreaterElement(vector<int>& nums) {
