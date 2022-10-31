78b5aa84aeaf01c073c8cdbe840068b48088301f
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-10-31 08:34:03 +0530

    Time: 32 ms (47.62%), Space: 17.5 MB (63.25%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -2,41 +2,14 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int n = matrix.size(), m =  matrix[0].size();
-        //first check if all starting points in j = 0 column work
-        //then check if all starting points in i = 0 work
-        //j == 0 and i == 0 overlap so only need to check that once
-        
-        bool ok = true;
-        
-        for(int i=n - 1; i >= 0; i--) {
-            int cur = matrix[i][0];
-            int ii = i, jj = 0;
-            while(ii + 1 < n && jj + 1 < m) {
-                if(matrix[ii + 1][jj + 1] != cur) {
-                    ok = false;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(i > 0 && j > 0 && matrix[i - 1][j - 1] != matrix[i][j]) {
+                    return false;
                 }
-                ii++;
-                jj++;
             }
         }
         
-        for(int j = 1; j<m; j++) {
-            int cur = matrix[0][j];
-            int ii = 0, jj = j;
-            while(ii + 1 < n && jj + 1 < m) {
-                if(matrix[ii + 1][jj + 1] != cur) {
-                    ok = false;
-                }
-                ii++;
-                jj++;
-            }
-        }
-        
-        if(ok) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return true;
     }
 };
