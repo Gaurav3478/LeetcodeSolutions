eacdc2928d2055f2b16deaba4ffebc253f50eb04
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-11-17 21:41:16 +0530

    Time: 374 ms (17.00%), Space: 59.6 MB (93.62%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,12 +4,13 @@ public:
         int n = arr.size();
         int low = 0, high = n - 1;
         while(low < high) {
-            int mid = low + (high - low)/2;
-            if(arr[mid] > arr[mid + 1]) {
-                high = mid;
+            int mid0 = low + (high - low)/2;
+            int mid1 = mid0 + 1;
+            if(arr[mid0] > arr[mid1]) {
+                high = mid0;
             }
             else {
-                low = mid + 1;
+                low = mid1;
             }
         }
         return low;
